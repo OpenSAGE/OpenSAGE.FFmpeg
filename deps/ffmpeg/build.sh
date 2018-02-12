79d9309f828037003ab3419ede6f706ed632e3f9
Stephan Vedder <stephan.vedder@gmail.com>
2018-02-12 21:32:34 +0100

    Rebuild ffmpeg to not require iconv or zlib. Also copy swresample, since we use that

--- deps/ffmpeg/build.sh
+++ deps/ffmpeg/build.sh
@@ -1,5 +1,5 @@
 ./configure --disable-everything --enable-decoder=mp3 --enable-demuxer=mp3 --enable-decoder=vp6 --enable-decoder=vp6a --enable-decoder=vp6f \
 --enable-demuxer=ea --enable-decoder=bink --enable-decoder=binkaudio_dct --enable-decoder=binkaudio_rdft --enable-demuxer=bink \
---enable-protocol=file --enable-decoder=adpcm_ea --enable-decoder=adpcm_ea_r3 --enable-shared
+--enable-protocol=file --enable-decoder=adpcm_ea --enable-decoder=adpcm_ea_r3 --enable-shared --disable-autodetect
 make -j4
 make install
